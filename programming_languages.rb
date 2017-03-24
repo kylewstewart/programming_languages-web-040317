1952c251ecce06181eea0ed17b6d07c9dd3c9cd3
Kyle Stewart <kyle.w.stewart@mac.com>
2017-03-24 20:52:18 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,36 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def languages_by_style
+  hash = {
+    oo: {
+      ruby: {type: "interpreted"},
+      javascript: {type: "interpreted"},
+      python: {type: "interpreted"},
+      java: {type: "compiled"}
+    },
+
+    functional: {
+      clojure: {type: "compiled"},
+      erlang: {type: "compiled"},
+      scala: {type: "compiled"},
+      javascript: {type: "interpreted"}
+    }
+  }
 end
+
+def reformat_languages(languages)
+  new_hash = {}
+
+  languages.each do |style, languages|
+    languages.each do |language, type|
+      if new_hash[language] == nil
+        new_hash[language] = {type: type[:type], style: [style]}
+      else
+        new_hash[language][:style] << style
+      end
+    end
+  end
+  new_hash
+end
+
+# puts reformat_languages
